2e183d553c04ec243f5cfa7496cbabb0757c8580
MahtabSinghAujla <125728731+MahtabSinghAujla@users.noreply.github.com>
2024-04-16 17:57:05 +0530

    Update sun.py

--- sun.py
+++ sun.py
@@ -1,49 +1,29 @@
-from bs4 import BeautiftrSoup
-import time
-import csv
+from bs4 import BeautifulSoup as bs
 import requests
+import pandas as pd
 
-start_url='https://en.wikipedia.org/wiki/List_of_brightest_stars'
-time.sleep(10)
+starsURL = 'https://en.wikipedia.org/wiki/List_of_brightest_stars'
 
-def scrape_more_data(hyperlink) :
-    try :
-        page=requests.get(hyperlink)
-        soup=BeautiftrSoup(page.content,'html.parser')
-        temp_list=[]
-        for tr_tag in soup.find_all('tr',attrs={'class':'fact_row'}):
-            td_tags=tr_tag.find_all('td')
-            for td_tag in td_tags :
-                try :
-                    temp_list.append(td_tag.find_all('div',attrs={'class':'value'})[0].contents[0])
-                except :
-                    temp_list.append('')
-        new_planets_data.append(temp_list)
-    except :
-        time.sleep(1)
-        scrape_more_data(hyperlink)
+page = requests.get(starsURL)
+print(page)
+soup = bs(page.text,'html.parser')
+star_table = soup.find('table')
 
-def scrape():
-    headers=['name','light_years_from_earth','star_mass','star_radius']
-    planet_data=[]
-    for i in range(0,428) :
-        soup=BeautiftrSoup(start_url,'html.parser')
-        for tr_tag in soup.find_all('tr',attrs={'class','exoplanet'}) :
-            td_tags=tr_tag.find_all('td')
-            temp_list=[]
-            for index,tr_tag in enumerate(td_tags) :
-                if index==0 :
-                    temp_list.append(tr_tag.find_all('a')[0].contents[0])
-                else :
-                    try :
-                        temp_list.append(tr_tag.content[0])
-                    except :
-                        temp_list.append('')
-            hyperlink_tr_tag=td_tags[0]
-            temp_list.append('https://en.wikipedia.com'+hyperlink_tr_tag.find_all('a',href='True')[0]['href'])
-            planet_data.append(temp_list)
-    with open('data.csv','w') as f :
-        csvwriter=csv.writer(f)
-        csvwriter.writerow(headers)
-        csvwriter.writerows(planet_data)
-scrape()
+temp_list= []
+table_rows = star_table.find_all('tr')
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+
+Name = []
+Distance =[]
+
+for i in range(1,len(temp_list)):
+    Name.append(temp_list[i][1])
+    Distance.append(temp_list[i][3])
+    
+df2 = pd.DataFrame(list(zip(Name,Distance)),columns=['Name','Distance'])
+print(df2)
+
+df2.to_csv('stars.csv')
